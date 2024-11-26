46824fff1a154ee1464e8f4068406df359559c94
Decse1 <148325245+Decse1@users.noreply.github.com>
2024-11-26 18:35:03 +0100

    47

---
+++ kaland_jatek_form/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace kaland_jatek_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.option4_rbtn = new System.Windows.Forms.RadioButton();
+            this.option3_rbtn = new System.Windows.Forms.RadioButton();
+            this.option2_rbtn = new System.Windows.Forms.RadioButton();
+            this.option1_rbtn = new System.Windows.Forms.RadioButton();
+            this.card_lbl = new System.Windows.Forms.Label();
+            this.exit_btn = new System.Windows.Forms.Button();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.health_lbl = new System.Windows.Forms.Label();
+            this.strenght_lbl = new System.Windows.Forms.Label();
+            this.luck_lbl = new System.Windows.Forms.Label();
+            this.continue_btn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SaddleBrown;
+            this.panel1.Controls.Add(this.option4_rbtn);
+            this.panel1.Controls.Add(this.option3_rbtn);
+            this.panel1.Controls.Add(this.option2_rbtn);
+            this.panel1.Controls.Add(this.option1_rbtn);
+            this.panel1.Controls.Add(this.card_lbl);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(470, 470);
+            this.panel1.TabIndex = 0;
+            // 
+            // option4_rbtn
+            // 
+            this.option4_rbtn.AutoSize = true;
+            this.option4_rbtn.Location = new System.Drawing.Point(19, 432);
+            this.option4_rbtn.MinimumSize = new System.Drawing.Size(0, 27);
+            this.option4_rbtn.Name = "option4_rbtn";
+            this.option4_rbtn.Size = new System.Drawing.Size(103, 27);
+            this.option4_rbtn.TabIndex = 4;
+            this.option4_rbtn.TabStop = true;
+            this.option4_rbtn.Text = "radioButton4";
+            this.option4_rbtn.UseVisualStyleBackColor = true;
+            // 
+            // option3_rbtn
+            // 
+            this.option3_rbtn.AutoSize = true;
+            this.option3_rbtn.Location = new System.Drawing.Point(19, 399);
+            this.option3_rbtn.MinimumSize = new System.Drawing.Size(0, 27);
+            this.option3_rbtn.Name = "option3_rbtn";
+            this.option3_rbtn.Size = new System.Drawing.Size(103, 27);
+            this.option3_rbtn.TabIndex = 3;
+            this.option3_rbtn.TabStop = true;
+            this.option3_rbtn.Text = "radioButton3";
+            this.option3_rbtn.UseVisualStyleBackColor = true;
+            // 
+            // option2_rbtn
+            // 
+            this.option2_rbtn.AutoSize = true;
+            this.option2_rbtn.Location = new System.Drawing.Point(19, 366);
+            this.option2_rbtn.MinimumSize = new System.Drawing.Size(0, 27);
+            this.option2_rbtn.Name = "option2_rbtn";
+            this.option2_rbtn.Size = new System.Drawing.Size(103, 27);
+            this.option2_rbtn.TabIndex = 2;
+            this.option2_rbtn.TabStop = true;
+            this.option2_rbtn.Text = "radioButton2";
+            this.option2_rbtn.UseVisualStyleBackColor = true;
+            // 
+            // option1_rbtn
+            // 
+            this.option1_rbtn.AutoSize = true;
+            this.option1_rbtn.Location = new System.Drawing.Point(19, 333);
+            this.option1_rbtn.MinimumSize = new System.Drawing.Size(0, 27);
+            this.option1_rbtn.Name = "option1_rbtn";
+            this.option1_rbtn.Size = new System.Drawing.Size(103, 27);
+            this.option1_rbtn.TabIndex = 1;
+            this.option1_rbtn.TabStop = true;
+            this.option1_rbtn.Text = "radioButton1";
+            this.option1_rbtn.UseVisualStyleBackColor = true;
+            // 
+            // card_lbl
+            // 
+            this.card_lbl.AutoSize = true;
+            this.card_lbl.BackColor = System.Drawing.SystemColors.Control;
+            this.card_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.card_lbl.Location = new System.Drawing.Point(15, 15);
+            this.card_lbl.Margin = new System.Windows.Forms.Padding(15);
+            this.card_lbl.MaximumSize = new System.Drawing.Size(440, 300);
+            this.card_lbl.MinimumSize = new System.Drawing.Size(440, 300);
+            this.card_lbl.Name = "card_lbl";
+            this.card_lbl.Size = new System.Drawing.Size(440, 300);
+            this.card_lbl.TabIndex = 0;
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.Location = new System.Drawing.Point(830, 442);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(100, 40);
+            this.exit_btn.TabIndex = 1;
+            this.exit_btn.Text = "Kilépés";
+            this.exit_btn.UseVisualStyleBackColor = true;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(724, 442);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(100, 40);
+            this.start_btn.TabIndex = 2;
+            this.start_btn.Text = "Indítás";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // health_lbl
+            // 
+            this.health_lbl.AutoSize = true;
+            this.health_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.health_lbl.Location = new System.Drawing.Point(495, 12);
+            this.health_lbl.Margin = new System.Windows.Forms.Padding(10, 5, 5, 5);
+            this.health_lbl.MaximumSize = new System.Drawing.Size(200, 30);
+            this.health_lbl.MinimumSize = new System.Drawing.Size(200, 30);
+            this.health_lbl.Name = "health_lbl";
+            this.health_lbl.Size = new System.Drawing.Size(200, 30);
+            this.health_lbl.TabIndex = 3;
+            this.health_lbl.Text = "Élet erő: ";
+            // 
+            // strenght_lbl
+            // 
+            this.strenght_lbl.AutoSize = true;
+            this.strenght_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.strenght_lbl.Location = new System.Drawing.Point(495, 51);
+            this.strenght_lbl.Margin = new System.Windows.Forms.Padding(10, 5, 5, 5);
+            this.strenght_lbl.MaximumSize = new System.Drawing.Size(200, 30);
+            this.strenght_lbl.MinimumSize = new System.Drawing.Size(200, 30);
+            this.strenght_lbl.Name = "strenght_lbl";
+            this.strenght_lbl.Size = new System.Drawing.Size(200, 30);
+            this.strenght_lbl.TabIndex = 4;
+            this.strenght_lbl.Text = "Harc erő:";
+            // 
+            // luck_lbl
+            // 
+            this.luck_lbl.AutoSize = true;
+            this.luck_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.luck_lbl.Location = new System.Drawing.Point(495, 90);
+            this.luck_lbl.Margin = new System.Windows.Forms.Padding(10, 5, 5, 5);
+            this.luck_lbl.MaximumSize = new System.Drawing.Size(200, 30);
+            this.luck_lbl.MinimumSize = new System.Drawing.Size(200, 30);
+            this.luck_lbl.Name = "luck_lbl";
+            this.luck_lbl.Size = new System.Drawing.Size(200, 30);
+            this.luck_lbl.TabIndex = 5;
+            this.luck_lbl.Text = "Szerencse:";
+            // 
+            // continue_btn
+            // 
+            this.continue_btn.Location = new System.Drawing.Point(488, 442);
+            this.continue_btn.Name = "continue_btn";
+            this.continue_btn.Size = new System.Drawing.Size(100, 40);
+            this.continue_btn.TabIndex = 6;
+            this.continue_btn.Text = "Tovább";
+            this.continue_btn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 493);
+            this.ControlBox = false;
+            this.Controls.Add(this.continue_btn);
+            this.Controls.Add(this.luck_lbl);
+            this.Controls.Add(this.strenght_lbl);
+            this.Controls.Add(this.health_lbl);
+            this.Controls.Add(this.start_btn);
+            this.Controls.Add(this.exit_btn);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(960, 540);
+            this.MinimumSize = new System.Drawing.Size(960, 540);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label card_lbl;
+        private System.Windows.Forms.Button exit_btn;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Label health_lbl;
+        private System.Windows.Forms.Label strenght_lbl;
+        private System.Windows.Forms.Label luck_lbl;
+        private System.Windows.Forms.RadioButton option4_rbtn;
+        private System.Windows.Forms.RadioButton option3_rbtn;
+        private System.Windows.Forms.RadioButton option2_rbtn;
+        private System.Windows.Forms.RadioButton option1_rbtn;
+        private System.Windows.Forms.Button continue_btn;
+    }
+}
+
---
+++ kaland_jatek_form/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace kaland_jatek_form
+{
+    public partial class Form1 : Form
+    {
+        class karakter
+        {
+            public int ugyesseg = 0;
+            public int eletero = 0;
+            public int szerencse = 0;
+            public int arany = 0;
+            public int etel = 0;
+            public int ekkovek = 0;
+            public List<string> felszereles = new List<string> { "Kard", "Bőrvért" };
+            public string[] italok = new string[2];
+
+
+        }
+        class kartya
+        {
+            public string szoveg = "";
+            public int kovetkezo = 0;
+            public kartya(string line)
+            {
+                string[] sk = line.Split(';');
+                this.szoveg = sk[0];
+                this.kovetkezo = int.Parse(sk[1]);
+            }
+        }
+
+        public static int dice()
+        {
+            int kocka = 0;
+            Random rnd = new Random();
+            kocka = rnd.Next(1, 7);
+            return kocka;
+        }
+
+        static kartya[] kartyak = new kartya[200];
+        static int[,] kartyaopciok = new int[5,200];
+
+        static karakter player = new karakter();
+
+        static int start_ugyesseg = 0;
+        static int start_eletero = 0;
+        static int start_szerencse = 0;
+        static int start_arany = 0;
+        static int start_etel = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region TXT import
+            StreamReader sr = new StreamReader("kartya.txt", Encoding.UTF8);
+            int kdb = 0;
+            int bdb = 1;
+            while (!sr.EndOfStream)
+            {
+                kartya tmp = new kartya(sr.ReadLine());
+                if (kdb != kartyak.Length && bdb % 3 == 0)
+                {
+                    
+                    kartyak[kdb] = tmp;
+                    kdb++;
+                }
+                bdb++;
+            }
+            sr.Close();
+            StreamReader sr2 = new StreamReader("kartya.txt", Encoding.UTF8);
+            int kdb2 = 0;
+            while (!sr2.EndOfStream)
+            {
+                string[] input = sr2.ReadLine().Split(';');
+                if (input.Length == 3)
+                {
+                    kartyaopciok[0, kdb2] = int.Parse(input[0]);
+                    kartyaopciok[1, kdb2] = int.Parse(input[1]);
+                    kartyaopciok[2, kdb2] = int.Parse(input[2]);
+                    kartyaopciok[3, kdb2] = 0;
+                    kartyaopciok[4, kdb2] = 0;
+                }
+                else if (input.Length == 4)
+                {
+                    kartyaopciok[0, kdb2] = int.Parse(input[0]);
+                    kartyaopciok[1, kdb2] = int.Parse(input[1]);
+                    kartyaopciok[2, kdb2] = int.Parse(input[2]);
+                    kartyaopciok[3, kdb2] = int.Parse(input[3]);
+                    kartyaopciok[4, kdb2] = 0;
+                }
+                else if (input.Length == 5)
+                {
+                    kartyaopciok[0, kdb2] = int.Parse(input[0]);
+                    kartyaopciok[1, kdb2] = int.Parse(input[1]);
+                    kartyaopciok[2, kdb2] = int.Parse(input[2]);
+                    kartyaopciok[3, kdb2] = int.Parse(input[3]);
+                    kartyaopciok[4, kdb2] = int.Parse(input[4]);
+                }
+
+            }
+            sr2.Close();
+            #endregion
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            start_ugyesseg = dice() + 6;
+            start_eletero = dice() + dice() + 12;
+            start_szerencse = dice() + 6;
+            start_arany = 20;
+            start_etel = 5;
+
+            player.ugyesseg = start_ugyesseg;
+            player.eletero = start_eletero;
+            player.szerencse = start_szerencse;
+            player.arany = start_arany;
+            player.etel = start_etel;
+
+            health_lbl.Text = $"Élet erő: {player.eletero}";
+            strenght_lbl.Text = $"Harc erő: {player.ugyesseg}";
+            luck_lbl.Text = $"Szerencse: {player.szerencse}";
+            card_lbl.Text = kartyak[0];
+        }
+    }
+}
